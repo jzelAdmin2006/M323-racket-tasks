bb79e1d96a034bad9ed788740a5c4a8e8b3420bb
Johannes Zeller <johannes_zeller@sluz.ch>
2023-09-07 19:02:53 +0200

    solve task 1.7

---
+++ Task1.7.rkt
@@ -0,0 +1,8 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname Task1.7) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
+(: house (number -> any))
+
+(define house (lambda (size) (above (triangle size "outline" "red") (square size "solid" "blue"))))
+
+(house 100)
