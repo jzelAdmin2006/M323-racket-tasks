d1501021759b2d240b0bb32c50f922fe6bb991ad
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-09-17 10:55:09 +0200

    solve task 2.7

---
+++ tasks/Task2.7.rkt
@@ -0,0 +1,23 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname Task2.7) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp")))))
+; Ein Haustier ist eins der folgenden:
+; - Hund
+; - Katze
+; - Schlange
+(define pet
+  (signature (one-of "dog" "cat" "snake")))
+
+; Ist ein Tier niedlich?
+(: cute? (pet -> boolean))
+
+(check-expect (cute? "dog") #t)
+(check-expect (cute? "cat") #t)
+(check-expect (cute? "snake") #f)
+
+(define cute?
+  (lambda (p)
+    (if
+      (or (string=? p "cat") (string=? p "dog"))
+      #t
+      #f)))
